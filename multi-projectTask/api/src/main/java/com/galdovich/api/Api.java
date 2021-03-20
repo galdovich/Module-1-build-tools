ae794b6986b446b602e370d91d5862b0fbc0cae4
Alexander Galdovich <galdovich95@gmail.com>
2021-03-20 09:59:26 +0300

    Modified Api class

--- multi-projectTask/api/src/main/java/com/galdovich/api/Api.java
+++ multi-projectTask/api/src/main/java/com/galdovich/api/Api.java
@@ -4,7 +4,7 @@ import com.galdovich.util.Utils;
 
 public class Api {
     public static void main(String[] args) {
-        System.out.println("Is all numbers are positive (-2, 6, 7, 2, 3): " +
+        System.out.println("Are all numbers even? (-2, 6, 7, 2, 3): " +
                 Utils.isAllPositiveNumbers("-2", "6", "7", "2", "3"));
     }
 }
